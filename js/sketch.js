e468d821d93b8a1728d851405934e294dc6a663c
readyletsgo <rich@rt80.net>
2021-04-08 09:14:07 -0400

    first p5 sketch

---
+++ js/sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  // Sets the screen to be 720 pixels wide and 400 pixels high
+  createCanvas(720, 400);
+  background(0);
+  noStroke();
+  fill(204);
+  triangle(18, 18, 18, 360, 81, 360);
+
+  fill(102);
+  rect(81, 81, 63, 63);
+
+}
